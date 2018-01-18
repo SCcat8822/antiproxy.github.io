73ed18cc750c75bf7383cab2d37df41f4cedb61b
SCcat8822 <huntercreations888@gmail.com>
2018-01-18 21:48:15 +0000

    Update and rename Index.php to index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+?>
+<html>
+<head>
+	<title>Anti Proxy Sign-in</title>
+</head>
+<body>
+	<table>
+		<tr>	
+			<form action="enter.php" method="post">
+				<td>
+					<input type="text" name="username">
+				</td>
+	<td><input type="password" name="password"></td>	
+	</tr>
+	</form>
+	</table>
+
+</body>
+</html>
