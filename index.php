4754dccf40256f281350457ee7cd8b65a5033e45
SCcat8822 <huntercreations888@gmail.com>
2018-01-18 22:02:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+if(isset($_POST['password'])){
+	if ($_POST['password'] == 'capitalproxyservice') {
+		echo "Welcome to the service!";
+		exit;
+	}else{
+		echo "Thank you, come again.";
+		exit;
+	}
+}
 ?>
 <html>
 <head>
@@ -11,7 +20,9 @@
 				<td>
 					<input type="text" name="username">
 				</td>
+
 	<td><input type="password" name="password"></td>	
+	<td><input type="submit" name="enter" value="enter"></td>
 	</tr>
 	</form>
 	</table>
